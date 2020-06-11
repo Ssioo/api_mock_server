368b425bc1204f4934493aa0bd680650e24c4a27
조연우 <wooisso@naver.com>
2020-06-11 15:22:24 +0900

    ADD Json dummmy list

--- routes/skku2020_capstone.js
+++ routes/skku2020_capstone.js
@@ -6,9 +6,22 @@ router.get('/', function(req, res, next) {
     var result = Object();
     result.imgUrl = "http://qweqwe.jpg";
     result.title = "원숭이와 자화상";
-    result.sizeX = 1080;
-    result.sizeY = 1920;
-    result.data = Array();
+    result.width = 1080;
+    result.height = 1920;
+    var data = Array();
+    for (var i = 0; i < 3; i++) {
+        var datum = Object();
+        datum.title = "나뭇잎";
+        datum.positionX = 13 * i;
+        datum.positionY = 97 * i;
+        datum.imgUrl = "http://sdadwaweas.jpg";
+        datum.width = 46;
+        datum.height = 87;
+        datum.soundUrl = "http://qweqweqwe.mp3";
+        data.push(datum);
+    }
+    result.data = data;
+
     res.send(result);
 });
 
