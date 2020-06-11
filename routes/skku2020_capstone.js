fad57a2d4417711b60d400c92017f2b59260af8a
조연우 <wooisso@naver.com>
2020-06-11 15:13:57 +0900

    ADD Json send test

--- routes/skku2020_capstone.js
+++ routes/skku2020_capstone.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET list - SKKU2020-Capstone */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    var result = Object();
+    result.imgUrl = "http://qweqwe.jpg";
+    result.title = "원숭이와 자화상";
+    result.sizeX = 1080;
+    result.sizeY = 1920;
+    result.data = Array();
+    stringify()
+    res.send(result);
 });
 
 module.exports = router;
